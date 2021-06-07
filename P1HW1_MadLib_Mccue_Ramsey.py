4d7d5662d09ff2d480601c3d259ead64062120e3
MccueRamsey <85509586+MccueRamsey@users.noreply.github.com>
2021-06-07 13:34:40 -0400

    Add files via upload
    Accepts multiple lines of input to do a mad lib

---
+++ P1HW1_MadLib_Mccue_Ramsey.py
@@ -0,0 +1,7 @@
+first_name = str(input())
+generic_location = str(input())
+whole_number = str(input())
+plural_noun = str(input())
+
+
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
