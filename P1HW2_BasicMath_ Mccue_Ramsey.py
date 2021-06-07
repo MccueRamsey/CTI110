1db2bf49f1767b09f537564cf81a57da7481ffc3
MccueRamsey <85509586+MccueRamsey@users.noreply.github.com>
2021-06-07 14:03:57 -0400

    Add files via upload
    Basic math script that gets a product and sum

---
+++ P1HW2_BasicMath_ Mccue_Ramsey.py
@@ -0,0 +1,37 @@
+# A basic math proccessing pyhton program
+    # 6/7/2021
+    # CTI-110 P1HW2 - Basic Math
+    # Ramsey Mccue
+
+numOne = input("Enter your first number :")
+numOne = float(numOne)
+
+numTwo = input("Enter your second number :")
+numTwo = float(numTwo)
+
+print("First number entered: " + str(numOne))
+print("Second number entered: " + str(numTwo))
+
+tSum = (numOne + numTwo)
+tProduct = (numOne * numTwo) 
+
+print("Sum of both numbers " + str(tSum))
+print("Product of both numbers " + str(tProduct))
+
+#Psuedo Code
+
+# num1 = prompt user input
+# num1 = cast num1 to float
+
+# num2 = prompt user input
+# num2 = cast num2 to float
+
+#print num1
+#print num2
+
+#Sum = num1 + num2
+#Product = num1 * num2
+
+#print Sum
+#Print Product
+
